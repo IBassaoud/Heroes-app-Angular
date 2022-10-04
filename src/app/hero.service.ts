8353a72522709f61e310d153698eb4c487d1a3a1
IBassaoud <ismail.bassaoud@gmail.com>
2022-10-04 21:38:51 +0000

    Added components CRUD operations hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,12 +9,36 @@ import { MessageService } from './message.service';
 
 // HTTP REQUEST
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'api/heroes'; // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
   constructor(
     private http: HttpClient,
@@ -28,22 +52,62 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  // getHeroes(): Observable<Hero[]> {
-  //   // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
-  //   const heroes = of(HEROES);
-  //   this.messageService.add('HeroService : fetched heroes');
-  //   return heroes;
-  // }
-
-  getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((x) =>
+        x.length
+          ? this.log(`found heroes matching "${term}"`)
+          : this.log(`no heroes matching "${term}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** GET heroes from the server */
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from "../hero";
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
 
@@ -9,21 +9,23 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 
 // Always export the component class so you can import it elsewhere … like in the AppModule.
 export class HeroesComponent implements OnInit {
-  heroes:  Hero[] = [];
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site
-  constructor(private heroService: HeroService, public messageService: MessageService) { 
+  constructor(
+    private heroService: HeroService,
+    public messageService: MessageService
+  ) {
     // When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
-  
-    // Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. 
+    // Reserve the constructor for minimal initialization such as wiring constructor parameters to properties.
     // The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
-    // Instead, call getHeroes() inside the ngOnInit lifecycle hook 
+    // Instead, call getHeroes() inside the ngOnInit lifecycle hook
     // and let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
   }
 
@@ -32,11 +34,26 @@ export class HeroesComponent implements OnInit {
     // It's a good place to put initialization logic.
     this.getHeroes();
   }
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent : Selected hero id=${hero.id}`);
-    console.log(this.selectedHero);    
+    console.log(this.selectedHero);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
   getHeroes(): void {
@@ -46,9 +63,8 @@ export class HeroesComponent implements OnInit {
     // Observable.subscribe() is the critical difference. The assignment occurs synchronously, as if the server could return heroes instantly or the browser could freeze the UI while it waited for the server's response.
 
     // That won't work when the HeroService is actually making requests of a remote server.
-    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
     // The new version waits for the Observable to emit the array of heroes, which could happen now or several minutes from now. The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.
     // This asynchronous approach works when the HeroService requests heroes from the server.
   }
-
 }
